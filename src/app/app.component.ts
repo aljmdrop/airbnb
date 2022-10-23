932c6ef59eeec7f0ef040fd315ea76fdda79bf40
daniel villamziar <danielalejandrovillamizar@gmail.com>
2022-10-22 22:14:36 -0300

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   constructor() {}
+  
+}
+
+export class ExampleComponent {
+  public data = ['Amsterdam', 'Buenos Aires', 'Cairo', 'Geneva', 'Hong Kong', 'Istanbul', 'London', 'Madrid', 'New York', 'Panama City'];
+  public results = [...this.data];
+
+  handleChange(event) {
+    const query = event.target.value.toLowerCase();
+    this.results = this.data.filter(d => d.toLowerCase().indexOf(query) > -1);
+  }
 }
